9220d5da47d7bde06739c448b160434242a76faf
Mariusz Hussariush <mariusz@curvelabs.eu>
2021-11-09 12:57:23 +0100

    Cleanup of default files

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,7 @@ import "./App.css";
 import MaxCurrencyInput from "./Components/Molecules/MaxCurrencyInput/MaxCurrencyInput";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MaxCurrencyInput
-          onChange={(e) => console.log(`e in app tsx`, e)}
-          type="number"
-          maxValue={0.666}
-          name={"this be monies"}
-        />
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
