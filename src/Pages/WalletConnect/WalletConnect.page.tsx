1c2cb309c21c57d45a524e4491ed17c59409a6a5
Mariusz Hussariush <mariusz@curvelabs.eu>
2021-11-11 10:36:05 +0100

    Removed state setting

--- src/Pages/WalletConnect/WalletConnect.page.tsx
+++ src/Pages/WalletConnect/WalletConnect.page.tsx
@@ -6,7 +6,6 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 import { useNavigate } from "react-router";
 
 const WalletConnectPage = () => {
-  const [web3, setWeb3] = useState<Web3 | null>(null);
   const navigate = useNavigate();
   useEffect(() => {
     wallettInit();
@@ -30,7 +29,6 @@ const WalletConnectPage = () => {
     const web3 = new Web3(provider);
     console.log(`web3`, web3);
     if (web3) {
-      setWeb3(web3);
       navigate("/");
     }
   };
