2ff68bfc46fdc39a28f79444036419ef3f635330
Mariusz Hussariush <mariusz@curvelabs.eu>
2021-11-15 14:39:16 +0100

    Navigation bar setup
    avat link using router now

---
+++ src/Components/Organisms/Navigation/Navigation.stories.tsx
@@ -0,0 +1,35 @@
+import Navigation from "./Navigation";
+import { Story, Meta } from "@storybook/react";
+import AvatarLink from "../../Atoms/AvatarLink/AvatarLink";
+import IconButton from "../../Atoms/IconButton/IconButton";
+import { DotsVerticalIcon } from "@heroicons/react/solid";
+import { BrowserRouter } from "react-router-dom";
+
+export default {
+  title: "Organisms/Navigation",
+  component: Navigation,
+  subcomponents: { AvatarLink, IconButton },
+} as Meta;
+
+const Template: Story = (args) => (
+  <BrowserRouter>
+    <Navigation
+      leftAligned={
+        <AvatarLink
+          path="/"
+          imgSrc="https://duckduckgo.com/assets/icons/meta/DDG-icon_256x256.png"
+        />
+      }
+      rightAligned={
+        <>
+          <IconButton onClick={() => {}}>
+            <DotsVerticalIcon className="h-6 w-6 text-black" />
+          </IconButton>
+        </>
+      }
+      {...args}
+    ></Navigation>
+  </BrowserRouter>
+);
+
+export const Default = Template.bind({});
---
+++ src/Components/Organisms/Navigation/Navigation.tsx
@@ -0,0 +1,18 @@
+import React, { FC, PropsWithChildren, ReactNode } from "react";
+import "./Navigation.css";
+export interface NavigationProps {
+  leftAligned: PropsWithChildren<ReactNode>;
+  rightAligned: PropsWithChildren<ReactNode>;
+}
+const Navigation: FC<NavigationProps> = ({ leftAligned, rightAligned }) => {
+  return (
+    <header className="outerNav">
+      <div className="innerNav">
+        <div className="flex">{leftAligned}</div>
+        <div className="flex">{rightAligned}</div>
+      </div>
+    </header>
+  );
+};
+
+export default Navigation;
