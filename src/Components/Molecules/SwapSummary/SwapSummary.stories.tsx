6733b0ff9299e2f35e3f250121c9185d64d5dbcb
Mariusz Hussariush <mariusz@curvelabs.eu>
2021-11-19 13:29:24 +0100

    Swap items with tooltips

--- src/Components/Molecules/SwapSummary/SwapSummary.stories.tsx
+++ src/Components/Molecules/SwapSummary/SwapSummary.stories.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import SwapSummary, { SwapSummaryProps } from "./SwapSummary";
 import { Story, Meta } from "@storybook/react";
 import SwapSummaryItem from "../SwapSummaryItem/SwapSummaryItem";
+import { QuestionMarkCircleIcon } from "@heroicons/react/solid";
+import ReactTooltip from "react-tooltip";
 
 export default {
   title: "Molecules/SwapSummary",
@@ -15,6 +17,40 @@ const Template: Story<SwapSummaryProps> = (args) => (
       value="0.15 SEC"
     ></SwapSummaryItem>
     <SwapSummaryItem label="You receive" value="0 CRD" />
+    <SwapSummaryItem label="You receive" value="0 CRD">
+      <>
+        <QuestionMarkCircleIcon
+          className="w-6 h-6 text-gray-400 ml-2"
+          data-tip
+          data-for="tooltip-trigger1"
+        />
+        <ReactTooltip
+          clickable={true}
+          id="tooltip-trigger1"
+          effect="solid"
+          place="bottom"
+        >
+          <span>This much</span>
+        </ReactTooltip>
+      </>
+    </SwapSummaryItem>
+    <SwapSummaryItem label="You also receive" value="0 CRD">
+      <>
+        <QuestionMarkCircleIcon
+          className="w-6 h-6 text-gray-400 ml-2"
+          data-tip
+          data-for="tooltip-trigger2"
+        />
+        <ReactTooltip
+          clickable={true}
+          id="tooltip-trigger2"
+          effect="solid"
+          place="bottom"
+        >
+          <span>This much also</span>
+        </ReactTooltip>
+      </>
+    </SwapSummaryItem>
   </SwapSummary>
 );
 
