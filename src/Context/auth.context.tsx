e63915f8020ab0504de9d2ec0d0c8bc4d3314c50
Mariusz Hussariush <mariusz@curvelabs.eu>
2021-11-24 09:55:29 +0100

    Login with endpoints

--- src/Context/auth.context.tsx
+++ src/Context/auth.context.tsx
@@ -7,10 +7,11 @@ import React, {
   useEffect,
 } from "react";
 import { useNavigate } from "react-router-dom";
+import { RequestUrl } from "../Consts/requestUrls";
 import { IUser } from "./user.context";
 
 export type AuthContext = {
-  login: () => void;
+  login: ({ email, password }: any) => void;
   logout: () => void;
   state: IUser | undefined;
   authError: Error | undefined;
@@ -39,21 +40,79 @@ const AuthProvider: FC<IAuthProvider> = (props) => {
     }
   };
 
-  const login = async (): Promise<void> => {
+  // try {
+  //   const res = await fetch(`${RequestUrl.BASE_URL}/auth/confirm_email`, {
+  //     method: "POST",
+  //     mode: "cors",
+  //     body: JSON.stringify({
+  //       email,
+  //       password,
+  //       token,
+  //     }),
+  //     headers: {
+  //       "Content-type": "application/json; charset=UTF-8",
+  //     },
+  //   });
+  //   if (res && !res.ok) {
+  //     const data = await res.json();
+  //     const message =
+  //       data.length > 1
+  //         ? data.reduce((acc: any, next: any) => {
+  //             const msg = `${acc.message} ${next.message}`;
+  //             return msg;
+  //           })
+  //         : data[0].message;
+
+  //     throw Error(message);
+  //   }
+  //   if (res.ok) {
+  //     setPassCreated(true);
+  //   }
+  // } catch (error) {
+  //   console.warn(error);
+  //   setError(error as Error);
+  // }
+
+  const login = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }): Promise<void> => {
     if (authError) {
       setAuthError(undefined);
     }
-
-    const testUserUrl = "https://jsonplaceholder.typicode.com/users";
     try {
-      const res = await fetch(testUserUrl);
-      const data = await res.json();
-      if (res.ok && data) {
-        setState(data[0]);
-        localStorage.setItem("user", JSON.stringify(data[0]));
+      const res = await fetch(`${RequestUrl.BASE_URL}/auth/login`, {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      if (res && !res.ok) {
+        const data = await res.json();
+        const message =
+          data.length > 1
+            ? data.reduce((acc: any, next: any) => {
+                const msg = `${acc.message} ${next.message}`;
+                return msg;
+              })
+            : data[0].message;
+
+        throw Error(message);
+      }
+      if (res.ok) {
+        // localStorage.setItem("user", JSON.stringify(data[0]));
+        // setState(data[0]);
       }
     } catch (error) {
-      setAuthError({ message: "Im an auth error" } as Error);
+      setAuthError(error as Error);
     }
   };
 
