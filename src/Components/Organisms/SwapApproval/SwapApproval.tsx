bba10c1d9236ba74db8147e7060ddc65db75ac46
Mariusz Hussariush <mariusz@curvelabs.eu>
2021-12-01 12:25:56 +0100

    Swap approval dialog

--- src/Components/Organisms/SwapApproval/SwapApproval.tsx
+++ src/Components/Organisms/SwapApproval/SwapApproval.tsx
@@ -1,6 +1,10 @@
-import React, { FC, useState } from "react";
+import { BigNumber, ethers } from "ethers";
+import React, { FC, useEffect, useState } from "react";
 import { useDialog } from "react-dialog-async";
+import { SECabi } from "../../../ABI/SECabi";
 import { Tokens } from "../../../Consts/tokens";
+import { useEthProvider } from "../../../Context/web3.context";
+import { TokenAddresses } from "../../../Enums/tokensAddresses";
 import Button from "../../Atoms/Button/Button";
 import "./SwapApproval.css";
 import SwapApprovalDialog from "./SwapApprovalDialog/SwapApprovalDialog";
@@ -11,13 +15,25 @@ export interface SwapApprovalProps {
 const SwapApproval: FC<SwapApprovalProps> = ({ token }) => {
   const confirmationDialog = useDialog(SwapApprovalDialog);
   const [dialogResult, setDialogResult] = useState<any>();
+  const { provider, signer } = useEthProvider();
+
   const otherToken = Tokens.find(({ name }) => name !== token)?.name;
-  console.log(`otherToken`, otherToken);
   const handleClick = async () => {
-    const response = await confirmationDialog
-      .show({ token, otherToken })
-      .catch((e) => console.log(`dialog error`, e));
-    setDialogResult(response);
+    const gasEstimate = await getEstimate().catch((e) =>
+      console.log(`gas estimate error`, e)
+    );
+    if (gasEstimate) {
+      const response = await confirmationDialog
+        .show({ token, otherToken, gasEstimate })
+        .catch((e) => console.log(`dialog error`, e));
+      setDialogResult(response);
+    }
+  };
+
+  const getEstimate = async (): Promise<BigNumber> => {
+    const contract = new ethers.Contract(TokenAddresses.SEC, SECabi, signer);
+    const amount = Number.MAX_SAFE_INTEGER - 1;
+    return await contract.estimateGas.approve(TokenAddresses.CRD, amount);
   };
 
   return (
@@ -29,7 +45,7 @@ const SwapApproval: FC<SwapApprovalProps> = ({ token }) => {
       <Button fullWidth variant="primary" onClick={handleClick}>
         Continue
       </Button>
-      <p className="text-grey-600 text-sm text-center mt-4">
+      <p className="text-gray-600 text-sm text-center mt-4">
         We will only ask you to do this once.
       </p>
     </section>
--- src/Components/Organisms/SwapApproval/SwapApprovalDialog/SwapApprovalDialog.tsx
+++ src/Components/Organisms/SwapApproval/SwapApprovalDialog/SwapApprovalDialog.tsx
@@ -1,10 +1,18 @@
+import { QuestionMarkCircleIcon, XIcon } from "@heroicons/react/solid";
+import { BigNumber, ethers } from "ethers";
 import React from "react";
 import { AsyncDialogProps } from "react-dialog-async";
+import ReactTooltip from "react-tooltip";
 import Button from "../../../Atoms/Button/Button";
-
+import Card from "../../../Atoms/Card/Card";
+import IconButton from "../../../Atoms/IconButton/IconButton";
+import SwapSummary from "../../../Molecules/SwapSummary/SwapSummary";
+import SwapSummaryItem from "../../../Molecules/SwapSummaryItem/SwapSummaryItem";
+import "./SwapApprovalDialog.css";
 interface SwapApprovalDialogProps {
   token?: string;
   otherToken?: string;
+  gasEstimate: BigNumber;
 }
 
 const ConfirmationDialog: React.FC<
@@ -13,35 +21,77 @@ const ConfirmationDialog: React.FC<
   return (
     <>
       {open && (
-        <div className={"absolute inset-0 flex items-start justify-center"}>
+        <div className={"swapApprovalDialog"}>
           <div
             className={"absolute w-full h-full bg-black bg-opacity-20"}
             onClick={() => handleClose()}
           ></div>
-          <div
-            className={
-              "relative mx-auto shadow p-4 bg-white z-10 rounded mt-16"
-            }
-          >
-            <h2 className="text-3xl font-semibold mb-5">
-              Delphia would like permission to access the {data.token} in your
-              wallet.
-            </h2>
-            <p className="text-sm font-normal mb-14">
-              This allows our app to swap your {data.token} with{" "}
-              {data.otherToken}. Changing permissions on the Ethereum blockchain
-              requires a transaction.
-            </p>
-            <Button
-              variant="primary"
-              fullWidth
-              onClick={() => handleClose(false)}
-            >
-              Confirm
-            </Button>
-            <p className="text-grey-600 text-sm text-center mt-4">
-              We will only ask you to do this once.
-            </p>
+          <div className={"swapApprovalDialog__contentWrapper"}>
+            <Card>
+              <header className="flex">
+                <h2 className="text-3xl font-semibold mb-5">
+                  Delphia would like permission to access the {data.token} in
+                  your wallet.
+                </h2>
+                <IconButton
+                  onClick={() => handleClose()}
+                  classes="ml-auto -mr-12 -mt-8"
+                >
+                  <XIcon className="w-8 h-8 " />
+                </IconButton>
+              </header>
+              <p className="text-sm font-normal mb-14">
+                This allows our app to swap your {data.token} with{" "}
+                {data.otherToken}. Changing permissions on the Ethereum
+                blockchain requires a transaction.
+              </p>
+              <Card variant="grey" classes="mb-9">
+                <p className="font-semibold text-lg pb-3 mb-3 border-gray-200 border-b">
+                  Transaction Details
+                </p>
+                <SwapSummary>
+                  <SwapSummaryItem
+                    label="Action"
+                    value={`Allow access to ${data.token}`}
+                  ></SwapSummaryItem>
+                  <SwapSummaryItem
+                    label="Estimated Gas Fee"
+                    value={`${ethers.utils.formatEther(data.gasEstimate)}`}
+                  >
+                    <>
+                      <QuestionMarkCircleIcon
+                        className="w-6 h-6 text-gray-400 ml-2"
+                        data-tip
+                        data-for="tooltip-trigger1"
+                      />
+                      <ReactTooltip
+                        clickable={true}
+                        id="tooltip-trigger1"
+                        effect="solid"
+                        place="bottom"
+                        className="max-w-sm"
+                      >
+                        <span>
+                          A required fee when making any transaction on the
+                          Ethereum blockchain, paid in ETH. Prices are dynamic
+                          based on network congestion.
+                        </span>
+                      </ReactTooltip>
+                    </>
+                  </SwapSummaryItem>
+                </SwapSummary>
+              </Card>
+              <Button
+                variant="primary"
+                fullWidth
+                onClick={() => handleClose(false)}
+              >
+                Confirm
+              </Button>
+              <p className="text-gray-600 text-sm text-center mt-4">
+                We will only ask you to do this once.
+              </p>
+            </Card>
           </div>
         </div>
       )}
